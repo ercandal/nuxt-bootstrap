a887da10ebae251c27d5dfcf3e822275f4189b55
ercandal <ercandall@hotmail.com>
2024-08-12 20:16:40 +0300

    switch to master branch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,10 @@ export default defineNuxtConfig({
   plugins: [
     { src: '~/plugins/bootstrap.js', mode: 'client' }
   ],
+  nitro: {
+    prerender: {
+      ignore: ['/**']
+    }
+  },
+  
 })
