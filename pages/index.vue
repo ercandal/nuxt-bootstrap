92d90e50252653e2717fe1cbe6705be595a391af
ercandal <ercandall@hotmail.com>
2024-08-12 19:43:10 +0300

    workflow permission set 'Read and write permissions'

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
         <NuxtLink to="/guest/categories/" class="btn btn-primary">Kategoriler</NuxtLink>
         <NuxtLink to="/guest/login" class="btn btn-primary">Giriş yap</NuxtLink>
         <NuxtLink to="/guest/signup" class="btn btn-primary">Kayıt ol</NuxtLink>
+        
     </div>
 </template>
 
