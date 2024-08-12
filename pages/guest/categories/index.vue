bbf8816a72b02c61c2131880bfa4049c90d877e8
ercandal <ercandall@hotmail.com>
2024-08-12 20:36:06 +0300

    deleted product pages for error

--- pages/guest/categories/index.vue
+++ pages/guest/categories/index.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <NuxtLink to="/" class="btn btn-primary">Anasayfa</NuxtLink>
-        <NuxtLink to="../categories/electronics" class="btn btn-primary">Elektronik</NuxtLink>
-        <NuxtLink to="../categories/hobbies" class="btn btn-primary">Bahçe & Hobiler</NuxtLink>
-        <NuxtLink to="../categories/books" class="btn btn-primary">Kitap</NuxtLink>
+        
     </div>
 </template>
 
